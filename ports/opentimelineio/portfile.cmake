12c3d95c26c0d52e5446314f2d066bd924169730
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2021-10-05 12:12:45 -0700

    trying to make otio work

--- ports/opentimelineio/portfile.cmake
+++ ports/opentimelineio/portfile.cmake
@@ -1,32 +1,42 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO PixarAnimationStudios/OpenTimelineIO
-    REF b104129ee01fc25745ab0d7f003c03e6accd6330 # v2.0.1
-    SHA512 51568e21eaf863747f67fbcffa7f42ba32f5892e8295dac6c9deb0f6205f57c231ea34ce028d84915e4be2f2773e362b74eaf057c2e4cf3ad4b60bf13a0b73db
+    REF v0.13
+    SHA512 2e11fa46e0fbb17568c578fc9619a2e9e2fdb9beb6838878f803ceb68754023d68a9c591f5c9308b5f557460beeae51c4374b970e9e63b74fb87be47221bd6b3
     HEAD_REF master
     PATCHES
-        # Add patches here
+        remove-pybind.patch
 )
 
-vcpkg_cmake_configure(
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH/src/deps/any
+    REPO thelink2012/any
+    REF f67bd5f8bbf7eb628bf38206d4ac5cb22438e6bb
+    SHA512 f6b251a6bb99e3ef4f46ecb49b4c0137f103271d37b78d359c6338a6d4a87d96110d0297a4f76ef0df40cdc56a08c297504f00242006f081eefddb8a2ff25fa4
+    HEAD_REF master
+    PATCHES
+        # Patches here
+)
+
+vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DOTIO_PYTHON_INSTALL:BOOL=OFF
         ${FEATURE_OPTIONS}
 )
 
-vcpkg_cmake_install()
+vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets()
 
 vcpkg_copy_pdbs()
 
 # Clean redundant files
-#file(REMOVE_RECURSE
-#    ${CURRENT_PACKAGES_DIR}/debug/include
-#    ${CURRENT_PACKAGES_DIR}/debug/share
-#    ${CURRENT_PACKAGES_DIR}/debug/OpenColorIOConfig.cmake
-#    ${CURRENT_PACKAGES_DIR}/OpenColorIOConfig.cmake
-#)
+file(REMOVE_RECURSE
+    ${CURRENT_PACKAGES_DIR}/debug/include
+    ${CURRENT_PACKAGES_DIR}/debug/share
+    ${CURRENT_PACKAGES_DIR}/debug/OpenTimelineIOConfig.cmake
+    ${CURRENT_PACKAGES_DIR}/OpenTimelineIOConfig.cmake
+)
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
